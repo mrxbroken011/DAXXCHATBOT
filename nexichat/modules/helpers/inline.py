712a46d5d960c92a45bc65664d3108d14c8211bd
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-06-07 21:03:04 +0530

    Update inline.py

--- nexichat/modules/helpers/inline.py
+++ nexichat/modules/helpers/inline.py
@@ -121,7 +121,6 @@ ABOUT_BTN = [
     ],
     [
         InlineKeyboardButton(text="🍾 ᴏᴡɴᴇʀ 🍾", user_id=OWNER),
-     #   InlineKeyboardButton(text="❄️ sᴏᴜʀᴄᴇ ❄️", callback_data="SOURCE"),
     ],
     [
         InlineKeyboardButton(text="🐳 ᴜᴘᴅᴀᴛᴇs 🐳", url=f"https://t.me/{UPDATE_CHNL}"),
